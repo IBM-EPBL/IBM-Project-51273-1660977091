d71b09e2109780177890ed50f2ac9aec010c1e4e
JennaJodly <114205714+JennaJodly@users.noreply.github.com>
2022-11-15 11:49:03 +0530

    Create Login.js

---
+++ Project development phase/Login.js
@@ -0,0 +1,19 @@
+
+import React from 'react'
+
+import { LoginForm } from '../components/forms/LoginForm'
+
+export const Login = () => {
+
+    const submitHandler=()=>{
+
+    }
+
+  return (
+    <div>
+<LoginForm/>
+    </div>
+  )
+}
+
+
